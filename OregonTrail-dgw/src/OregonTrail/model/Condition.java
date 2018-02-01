0ff0ac3c03e98517a94c9ddd108fbd51bab2217f
Marci <Marci@10.0.0.2>
2018-02-01 15:15:48 -0700

    Week 4: Set up and First Class, Marci's  Classes, with Instances

--- OregonTrail-dgw/src/OregonTrail/model/Condition.java
+++ OregonTrail-dgw/src/OregonTrail/model/Condition.java
@@ -7,6 +7,7 @@ package OregonTrail.model;
 
 //import Serializable
 import java.io.Serializable;
+import java.util.Objects;
 /**
  *
  * @author Marci
@@ -15,5 +16,54 @@ public class Condition implements Serializable {
     
     //Class Instance Variables
     private String excute;
+
+    //Constructor
+    public Condition() {
+    }
+
+    
+    //Getters & Setters
+    public String getExcute() {
+        return excute;
+    }
+
+    public void setExcute(String excute) {
+        this.excute = excute;
+    }
+    
+    
+    //equals & Hashcode
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 17 * hash + Objects.hashCode(this.excute);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Condition other = (Condition) obj;
+        if (!Objects.equals(this.excute, other.excute)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    //toString
+    @Override
+    public String toString() {
+        return "Condition{" + "excute=" + excute + '}';
+    }
+    
     
 }//closing tag
