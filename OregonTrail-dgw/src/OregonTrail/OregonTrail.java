76ee99903b967f22cdf84f852df0bd6b7d1e2cf4
Marci <Marci@10.0.0.2>
2018-02-03 14:11:05 -0700

    Week 4: Marci's  Classes, with everything done, Couldn't test, because I don't know how and instructions do not make sense.

--- OregonTrail-dgw/src/OregonTrail/OregonTrail.java
+++ OregonTrail-dgw/src/OregonTrail/OregonTrail.java
@@ -5,6 +5,9 @@
  */
 package OregonTrail;
 
+import OregonTrail.model.ScenicStop;
+
+
 /**
  *
  * @author davidgw
@@ -15,9 +18,26 @@ public class OregonTrail {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*
+        This is Marci, trying to figure out how to test. 
+        I don't know how and the instructions don't make sense. 
+        The tutor will teach me how. 
+        
+        ScenicStop stopLocationOne = new ScenicStop;
+        
+        ScenicStop.location1.getNameOfStop();
+        ScenicStop.location1.getAboutStop();
+        
+        String locationOneName = .getNameOfStop();
+       
+        System.out.println( "Where are you? " + NameOfStop + "Learn about your new location: " + );
+          */      
+                
         // TODO code application logic here
         System.out.println("This is the first line of the Oregon Trail program!");
         System.out.println("One more change and commit to test.");
+        
+        
     }
     
 }
