c90b909c1a513de34f8ec104c1c303c39d7da578
Marci <Marci@10.0.0.2>
2018-02-03 14:25:34 -0700

    Week 4: Marci's  Classes, with everything done, Couldn't test, going to learn with tutor next week.

--- OregonTrail-dgw/src/OregonTrail/OregonTrail.java
+++ OregonTrail-dgw/src/OregonTrail/OregonTrail.java
@@ -18,24 +18,11 @@ public class OregonTrail {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*
-        This is Marci, trying to figure out how to test. 
-        I don't know how and the instructions don't make sense. 
-        The tutor will teach me how. 
-        
-        ScenicStop stopLocationOne = new ScenicStop;
-        
-        ScenicStop.location1.getNameOfStop();
-        ScenicStop.location1.getAboutStop();
-        
-        String locationOneName = .getNameOfStop();
-       
-        System.out.println( "Where are you? " + NameOfStop + "Learn about your new location: " + );
-          */      
-                
-        // TODO code application logic here
-        System.out.println("This is the first line of the Oregon Trail program!");
-        System.out.println("One more change and commit to test.");
+      /*not correct but thats because I don't understand how to test. 
+      //Instructions don't describe what is what and what they do. Retest next week with tutor.
+      System.out.println(ScenicStop.locationOne.getNameOfStop());
+       System.out.println(ScenicStop.LocationOne.getAboutStop()); 
+        */
         
         
     }
