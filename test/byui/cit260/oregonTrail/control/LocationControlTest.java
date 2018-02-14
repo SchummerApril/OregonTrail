02225dbb377ab06f0d58154da8d486078139a8f1
aschummer <aschummer@aprils-mbp.satx.rr.com>
2018-02-14 14:17:03 -0700

    Classes April Schummer

---
+++ test/byui/cit260/oregonTrail/control/LocationControlTest.java
@@ -0,0 +1,50 @@
+package byui.cit260.oregonTrail.control;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author aschummer
+ */
+public class LocationControlTest {
+    
+    public LocationControlTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcMilesTraveled method, of class LocationControl.
+     */
+    @Test
+    public void testCalcMilesTraveled() {
+        System.out.println("calcMilesTraveled");
+        int startingPosition = 0;
+        int currentPosition = 50;
+        double expResult = 50.00;
+        double result = LocationControl.calcMilesTraveled(startingPosition, currentPosition);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+            
+        System.out.println("invalid1 TotalMilesTraveled");
+        int startingPosition = 30;
+        int currentPosition = 50;
+        double expResult = 80.00;
+        double result = LocationControl.calcMilesTraveled(startingPosition, currentPosition);
+        assertEquals(expResult, result, -1.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+
+    }
+    
+}
