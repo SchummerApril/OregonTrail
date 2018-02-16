a01c4acd70c6dc7afe02578404926b4ad0d47fdd
Marci <Marci@10.0.0.2>
2018-02-16 11:03:07 -0700

    WK6.Marci G.

--- src/byui/cit260/oregonTrail/control/MapControl.java
+++ src/byui/cit260/oregonTrail/control/MapControl.java
@@ -1,4 +1,6 @@
 package byui.cit260.oregonTrail.control;
+import byui.cit260.oregonTrail.model.Map;
+import byui.cit260.oregonTrail.model.Point;
 
 /**
  *          Main Author, Classmate, Classmate
@@ -7,7 +9,7 @@ package byui.cit260.oregonTrail.control;
 public class MapControl {
     
     //How to move the player Part 1
-    public static boolean isValidPoint(MapControl map, Point p) {
+    public static boolean isValidPoint(Map map, Point p) {
             for (Point existing : map.getContents().keySet()) {
                 if (existing.getY() == p.getY() && existing.getX() == p.getX()) {
                     return true;
@@ -19,8 +21,8 @@ public class MapControl {
 
 
         //How to move the player Part 2
-        public static boolean movePlayer(MapControl map, Point destination) {
-            if (GameMapControl.isValidPoint(map, destination)) {
+        public static boolean movePlayer(Map map, Point destination) {
+            if (MapControl.isValidPoint(map, destination)) {
                 map.setPlayerLocation(destination);
                 return true;
             }
@@ -30,13 +32,17 @@ public class MapControl {
 
 
         // How you get the distance between the Player and Stop
-        public static double distancePlayerToPoint(MapControl map, Point p2) {
+        public static double distancePlayerToPoint(Map map, Point p2) {
             if (!MapControl.isValidPoint(map, p2)) {
                 return -1.0;
             }
 
             Point p1 = map.getPlayerLocation();
-
+            int x1 = p1.getX();
+            int y1 = p1.getY();
+            
+            int x2 = p2.getX();
+            int y2 = p2.getY();
             // calculate distance using distance formula
             double distance = Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2));
 
@@ -45,16 +51,12 @@ public class MapControl {
         }
 
 
-        //Location of Stop
-        public static void visitCurrentLocation(MapControl map) {
-            Point currentPoint = map.getPlayerLocation();
-            Location currentLocation = map.getContents().get(currentPoint);
-            currentLocation.visit();
+        /*Location of Stop //Reference how to do it
+        *public static void visitCurrentLocation(Map map) {
+        *    Point currentPoint = map.getPlayerLocation();
+        *   Location currentLocation = map.getContents().get(currentPoint);
+        *    currentLocation.visit();
         }
+           */
 
-        //The Current Location of Player
-        // Superfluous: just use map.getPlayerLocation() outside the control layer!
-        public static Point getPlayerLocation(MapControl map) {
-            return map.getPlayerLocation();
-        }
 }
--- src/byui/cit260/oregonTrail/model/Map.java
+++ src/byui/cit260/oregonTrail/model/Map.java
@@ -1,5 +1,6 @@
 package byui.cit260.oregonTrail.model;
 
+import java.util.HashMap;
 import java.io.Serializable;
 import java.util.Objects;
 /**
@@ -7,11 +8,31 @@ import java.util.Objects;
  * @author aschummer, Marci, Nastia
  */
 public class Map implements Serializable{
+    private HashMap<Point, Location> contents = new HashMap<Point,Location>(); //List that maps(connects) points to location
     
     private String location;
-    private int rowCount;
+    private int rowCount; //Check for another way of doing this....LATER
     private int columnCount;
+    
+    Point playerLocation;
 
+    public HashMap<Point, Location> getContents() {
+        return contents;
+    }
+
+    public void setContents(HashMap<Point, Location> contents) {
+        this.contents = contents;
+    }
+
+    
+    public Point getPlayerLocation() {
+        return playerLocation;
+    }
+
+    public void setPlayerLocation(Point playerLocation) {
+        this.playerLocation = playerLocation;
+    }
+    
     public String getLocation() {
         return location;
     }
