98559dc3fe1b85cbc1ba64eb82f8544c847022eb
aschummer <aschummer@aprils-mbp.satx.rr.com>
2018-02-19 15:37:40 -0700

    Merge origin/master
    Conflicts:
    	src/byui/cit260/oregonTrail/model/ScenicStop.java
    	src/byui/cit260/oregonTrail/model/Stop.java

--- src/byui/cit260/oregonTrail/control/GameControl.java
+++ src/byui/cit260/oregonTrail/control/GameControl.java
@@ -1,9 +1,11 @@
 package byui.cit260.oregonTrail.control;
-
+import byui.cit260.oregonTrail.model.Player;
 /**
  *          Main Author, Classmate, Classmate
  * @author aschummer
  */
-public class GameControl {
-    
+public abstract class GameControl {
+    public static Player createNewPlayer(String name){
+        return new Player(name);
+}
 }
