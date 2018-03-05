a4becee3e99f4835bf7330773529b9f72cb91aa5
aschummer <aschummer@aprils-mbp.fios-router.home>
2018-03-05 13:21:26 -0700

    Everything is working now fixed startExistingGame

--- src/byui/cit260/oregonTrail/model/Container.java
+++ src/byui/cit260/oregonTrail/model/Container.java
@@ -7,5 +7,34 @@ package byui.cit260.oregonTrail.model;
 public class Container {
     private int weight;
     private int maxCarryWeight;
+    private Inventory contents;
+
+    public Container() {
+        this.contents = new Inventory();
+    }
+
+    public Inventory getContents() {
+        return contents;
+    }
+
+    public void setContents(Inventory contents) {
+        this.contents = contents;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getMaxCarryWeight() {
+        return maxCarryWeight;
+    }
+
+    public void setMaxCarryWeight(int maxCarryWeight) {
+        this.maxCarryWeight = maxCarryWeight;
+    }
     
 }
